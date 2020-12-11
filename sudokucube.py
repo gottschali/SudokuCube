5c5e8001d6af5bc80f52e560e8d4f6ed5290a53c
Aliukabar <aligottschall22@gmail.com>
2020-12-11 23:30:51 +0100

    Refactor SudokuCube

--- sudokucube.py
+++ sudokucube.py
@@ -7,23 +7,26 @@ from helper import human_step_by_step
 class SudokuCube(dict):
 
     def __init__(self):
-        self._sequence = Sequence()
-        self._faces = Faces()
+        self.sequence = Sequence()
+        self.faces = Faces()
 
     def solve(self):
-        # Solve from all possible starting points
         solutions = 0
+        # Solve from all distinct starting points, all others are symmetrical
         for coord in (Coordinate(x, y, z) for x, y, z in ((0, 0, 0), (0, 0, 1), (0, 1, 1), (1, 1, 1))):
-            if self._solve(coord, 0):
+            if self._solve(coord, 0): # A solution is found
                 human_step_by_step(self)
                 solutions += 1
         print(f"Found {solutions} solutions")
+        return solutions
 
     def _solve(self, coordinate, depth):
-        # Update the temporary solution
-        self[coordinate] = index, color = self._sequence.push()
-        if self._faces.test_color_constraint(coordinate, color):
-            self._faces.add_dice(coordinate, color)
+        # Step in: go forward in the sequence
+        dice = self.sequence.push()
+        if self.faces.test_color_constraint(coordinate, dice.color):
+            # Step in: set the cube map and add the colors to the faces
+            self[coordinate] = dice
+            self.faces.add_dice(coordinate, dice.color)
             # Recursively solve for every neighbor
             for neighbor in coordinate.neighbors():
                 # Test that the neigbor is not already occupied
@@ -31,17 +34,16 @@ class SudokuCube(dict):
                     if self._solve(neighbor, depth + 1):
                         # Propagate a solution
                         return True
-            # Remove the colors from the faces
-            self._faces.remove_dice(coordinate, color)
-        # Condition for termination
-        if self._sequence.done():
-            # Backtrack
-            self._sequence.pop()
+            # Backtrack: remove the colors from the faces and reset the cube map
+            self.faces.remove_dice(coordinate, dice.color)
             del self[coordinate]
+        # Condition for termination
+        if self.sequence.done():
+            # Backtrack
+            self.sequence.pop()
             return True
-        # Backtrack
-        self._sequence.pop()
-        del self[coordinate]
+        # Backtrack: go back in the sequence
+        self.sequence.pop()
 
 
 if __name__ == "__main__":
