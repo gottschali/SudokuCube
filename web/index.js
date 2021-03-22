af6da81e9ddc3d82206fea0949fbe0145718cb23
Aliukabar <aligottschall22@gmail.com>
2021-03-22 21:34:24 +0100

    Remove unneeded lines

--- web/index.js
+++ web/index.js
@@ -26,7 +26,6 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 // Append Renderer to DOM
 document.body.appendChild( renderer.domElement );
 
-
 const diceGeometry = new THREE.BoxGeometry( 1, 1, 1 );
 
 const colors = [
@@ -47,59 +46,32 @@ colors.forEach((c, i) => dices[i] = new THREE.Mesh(diceGeometry, new THREE.MeshB
 const cube = new THREE.Group();
 scene.add( cube );
 
-// Render Loop
-// var render = function () {
-  // requestAnimationFrame( render );
-
-  // Render the scene
-  // renderer.render(scene, camera);
-// };
-
 function render(){
-  // window.requestAnimationFrame( animation );
-
   cube.rotation.x = Date.now() * 0.00005;
   cube.rotation.y = Date.now() * 0.0001;
 
   renderer.render( scene, camera);
 }
 
-const sleep = (milliseconds) => { return new Promise(resolve => setTimeout(resolve, milliseconds));};
-
-function pause(millis)
-{
-    var date = new Date();
-    var curDate = null;
-    do { curDate = new Date(); }
-    while(curDate-date < millis);
-}
 
 let history = [];
 
 function move_cube(x, y, z, color) {
     const dice = dices[color - 1].clone();
-    // var dice = new THREE.Mesh(diceGeometry, new THREE.MeshBasicMaterial( {color: colors[color]}));
-
 	  history.push(dice)
     cube.add(dice);
-
 	  dice.position.set(x, y, z);
     // render();
 }
 
 
-// Do lookup by name instead of hist array
-//function removeEntity(object) {
-    //var selectedObject = scene.getObjectByName(object.name);
-    //scene.remove( selectedObject );
-    //animate();
-//}
 
 function do_step(x, y, z, color) {
     if (color != 0) {
         move_cube(x, y, z, color);
     } else {
 			// probably means remove
+// Do lookup by name instead of hist array
 			const dice = history.pop();
 			cube.remove( dice );
         console.log("nothing");
@@ -115,13 +87,6 @@ function run() {
 	 render();
 	 i++;
 }
-
-//steps.forEach(([[x, y, z], color]) => {
-	//do_step(x, y, z, color);
-	//render();
-//});
-// do_step(1, 0, 0, 1);
-// do_step(-1, 1, 0, 2);
 
 window.setInterval( run, 1 );
 
