2158d66dcac573a6ab8201ba371f5049434eddaa
Aliukabar <aligottschall22@gmail.com>
2021-03-24 20:12:17 +0100

    Disallowed MIME type

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import {steps} from '../steps.js';
+import {steps} from './steps.js';
 
 // Automatic resizing
 
