43246b76eb23d837db86367479ae31a573b474bf
Ali <aligottschall22@gmail.com>
2021-03-24 15:12:40 +0000

    Enum

--- solver/constant.py
+++ solver/constant.py
@@ -1,33 +1,27 @@
-from collections import namedtuple
+from enum import IntEnum 
+from coordinate import Coordinate
 
-# Datastructure for holding the cubes of the SudokuCube
-Dice = namedtuple("Dice", ["index", "color"])
+colors = ("cyan", "green", "yellow",
+          "pink", "blue", "black",
+          "red", "brown", "grey")
 
-# Define constants for the colors of the dice
-CYAN = 1
-GREEN = 2
-YELLOW = 3
-PINK = 4
-BLUE = 5
-BLACK = 6
-RED = 7
-BROWN = 8
-GREY = 9
+Color = IntEnum("Color", colors)
 
-ORIGINAL_COLORS = tuple(Dice(i, color) for i, color in enumerate([
-        CYAN, GREEN, YELLOW, PINK, BLUE, BLACK, RED, GREEN, CYAN, BLUE, BLACK,
-        BROWN, GREY, YELLOW, RED, CYAN, BLUE, BROWN, BLACK, RED, GREY, BROWN,
-        PINK, GREEN, YELLOW, GREY, PINK
-    ]))
+class Seq(tuple):
 
-# To look up the names back. Numbers are used in the first place to optimize memory
-COLOR_MAP = {CYAN : "cyan",
-             GREEN : "green",
-             YELLOW : "yellow",
-             PINK : "pink",
-             BLUE : "blue",
-             BLACK : "black",
-             RED : "red",
-             BROWN : "brown",
-             GREY : "grey"}
+    def __init__(self, iterable=(), /):
+        self = tuple(iterable)
+        if not len(self)**(1/3).is_integer():
+            raise ValueError("Length of sequence must be a power of three")
 
+OG = Seq(getattr(Color, color) for color in (
+        "cyan", "green", "yellow",
+        "pink", "blue", "black",
+        "red", "green", "cyan",
+        "blue", "black", "brown",
+        "grey", "yellow", "red",
+        "cyan", "blue", "brown",
+        "black", "red", "grey",
+        "brown", "pink", "green",
+        "yellow", "grey", "pink"
+    ))
